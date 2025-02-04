0271f8f99b6284db703d530ee31719231d7f7077
Electronic Payments <40696037+ElectronicPaymentsInc@users.noreply.github.com>
2025-02-04 11:42:47 -0500

    Add files via upload

--- procharge/src/main/java/com/electronicpaymentsinc/procharge/AuthResponse.kt
+++ procharge/src/main/java/com/electronicpaymentsinc/procharge/AuthResponse.kt
@@ -3,9 +3,23 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class AuthResponse (
-    val access_token: String = "",
-    val refresh_token: String = "",
-    val lastLogin: String = "",
+    var access_token: String = "",
+    var refresh_token: String = "",
+    var lastLogin: String = "",
+    var responseText: String? = "",
     var statusCode: Int = 200,
-    var responseText: String? = ""
+    var businessName: String = "",
+    var firstName: String = "",
+    var lastName: String = "",
+    var groups: List<String> = emptyList<String>(),
+    var email: String = "",
+    var merchantNumber: String = "",
+    var paymentGatewayID: String = "",
+    var merchantID: Int = 0,
+    var profileID: Int = 0,
+    var profileName: String = "",
+    var acquirerID: String = "",
+    var terminalID: String = "",
+    var createdDate: String = "",
+    var industryType: Int = 0
 )
